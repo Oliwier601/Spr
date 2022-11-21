a6d65505bc39437aa8e8beffd33c7dcf51bf9c22
Oliwier601 <113879766+Oliwier601@users.noreply.github.com>
2022-11-21 08:21:15 +0100

    Update spr.cs

--- spr.cs
+++ spr.cs
@@ -22,23 +22,10 @@ else
 }
 
 //Zad 3
-//int n = int.Parse(Console.ReadLine());
-/*for (int i = 0; i < n; i++)
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j <= i; j++)
-    {
-        Console.Write(" ");
-    }
-    for (int k = 0; k <= i; k++)
-    {
-        Console.Write("*");
-    }
-    Console.WriteLine();
-
-}*/
-/*for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n-i; j++)
+    for (int j = 0; j < n-i-1; j++)
     {
         Console.Write(" ");
     }
@@ -47,4 +34,12 @@ else
         Console.Write("*");
     }
     Console.WriteLine();
-}*/
+}
+for (int i = 0; i < n/2; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
